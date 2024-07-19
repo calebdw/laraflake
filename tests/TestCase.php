987a896744011a0be9af3e68094a939096f6cf88
Caleb White <cdwhite3@pm.me>
2024-07-18 21:53:47 -0500

    test: remove WithMigration from TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace CalebDW\Laraflake\Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Orchestra\Testbench\Attributes\WithMigration;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
-#[WithMigration]
 abstract class TestCase extends OrchestraTestCase
 {
     use RefreshDatabase;
