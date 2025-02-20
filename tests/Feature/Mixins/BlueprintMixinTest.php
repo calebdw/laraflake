1decd032f66f8afd11be2ce6c7ceefa019f497fd
Caleb White <cdwhite3@pm.me>
2025-02-20 05:26:27 -0600

    fix: update test for 12.x

--- tests/Feature/Mixins/BlueprintMixinTest.php
+++ tests/Feature/Mixins/BlueprintMixinTest.php
@@ -7,17 +7,25 @@ use Illuminate\Database\Schema\Grammars\SQLiteGrammar;
 use Illuminate\Support\Facades\DB;
 use Workbench\App\Models\User;
 
+use function Orchestra\Testbench\laravel_version_compare;
+
 beforeEach(function () {
-    test()->blueprint = new Blueprint('snowflake');
+    test()->blueprint = resolve(Blueprint::class, ['table' => 'snowflake']);
+
+    test()->sql = function () {
+        if (laravel_version_compare('12.0', '>=')) {
+            return test()->blueprint->toSql();
+        }
+
+        return test()->blueprint->toSql(DB::connection(), new SQLiteGrammar());
+    };
 });
 
 it('adds snowflake column', function () {
     test()->blueprint->snowflake();
     test()->blueprint->snowflake('foo');
 
-    $sql = test()->blueprint->toSql(DB::connection(), new SQLiteGrammar());
-
-    expect($sql)->toBe([
+    expect((test()->sql)())->toBe([
         'alter table "snowflake" add column "id" integer not null',
         'alter table "snowflake" add column "foo" integer not null',
     ]);
@@ -26,9 +34,7 @@ it('adds snowflake column', function () {
 it('adds foreign snowflake column', function () {
     test()->blueprint->foreignSnowflake('user_id');
 
-    $sql = test()->blueprint->toSql(DB::connection(), new SQLiteGrammar());
-
-    expect($sql)->toBe([
+    expect((test()->sql)())->toBe([
         'alter table "snowflake" add column "user_id" integer not null',
     ]);
 });
@@ -36,9 +42,7 @@ it('adds foreign snowflake column', function () {
 it('adds foreign snowflake for a model', function () {
     test()->blueprint->foreignSnowflakeFor(User::class);
 
-    $sql = test()->blueprint->toSql(DB::connection(), new SQLiteGrammar());
-
-    expect($sql)->toBe([
+    expect((test()->sql)())->toBe([
         'alter table "snowflake" add column "user_id" integer not null',
     ]);
 });
