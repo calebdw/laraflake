25198f5ffb1a9a8423063004eae7ed434232fc6a
Caleb White <cdwhite3@pm.me>
2026-08-25 12:16:22 -0500

    chore: switch to calebdw/phpstan-laravel

--- workbench/database/migrations/2024_07_16_203215_create_user_and_post_table.php
+++ workbench/database/migrations/2024_07_16_203215_create_user_and_post_table.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Workbench\App\Models\User;
 
-return new class extends Migration
+return new class () extends Migration
 {
     public function up(): void
     {
